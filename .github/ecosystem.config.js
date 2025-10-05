e37a8d1c82f35c9737d8e66dfc26c0fd51d33768
ZERK <konesesesa@gmail.com>
2025-10-05 17:39:30 +0300

    Create ecosystem.config.js

---
+++ .github/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "sms-activate",
+      script: "index.js",
+      cwd: __dirname + "/sms-activate",
+      autorestart: true
+    },
+    {
+      name: "tech-god-bug",
+      script: "main.js",
+      cwd: __dirname + "/tech-god-bug",
+      autorestart: true
+    },
+    {
+      name: "root-index",
+      script: "index.js",
+      cwd: __dirname + "/root-index",
+      autorestart: true
+    }
+  ]
+};
